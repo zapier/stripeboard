c14ea746dfbe04b66e6fbb7184e55d0041360f97
Bryan Helmig <bryan@bryanhelmig.com>
2012-08-19 04:20:05 -0700

    remove silly asset serving

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: python manage.py collectstatic --noinput; gunicorn stripeboard.wsgi:application -b 0.0.0.0:$PORT
+web: gunicorn stripeboard.wsgi:application -b 0.0.0.0:$PORT
 celeryd: python manage.py celeryd -E -B --loglevel=INFO
--- stripeboard/urls.py
+++ stripeboard/urls.py
@@ -12,6 +12,4 @@ urlpatterns = patterns('',
 
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
-
-    url(r'^static/(?P.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
 )
