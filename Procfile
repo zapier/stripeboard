58463edcc195c72315241a401aecfcc3d05ce9d9
Bryan Helmig <bryan@bryanhelmig.com>
2012-08-19 13:34:34 -0700

    static serving updates

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 web: gunicorn stripeboard.wsgi:application -b 0.0.0.0:$PORT
-celeryd: python manage.py celeryd -E -B --loglevel=INFO
+celeryd: python manage.py celeryd -E -B --concurrency=1 --loglevel=INFO
--- stripeboard/urls.py
+++ stripeboard/urls.py
@@ -12,4 +12,6 @@ urlpatterns = patterns('',
 
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
 )
